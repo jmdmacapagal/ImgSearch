a08ce9740303b21209d16131b7b6dccd2ca548eb
jm macapagal <jmdmacapagal@gmail.com>
2019-06-17 19:31:37 +0800

    rewrite of React ref

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
 
 class ImageCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { spans: 0 }
+        this.imageRef = React.createRef()
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans)
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight
+        const spans = Math.ceil(height / 10)
+
+        this.setState({
+            spans
+        })
+    }
+
    render() {
        const { urls, description } = this.props.images
     return (
-        <div>
+        <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
             <img
+                ref={this.imageRef}
                 src={urls.small}
                 alt={description}
             />
